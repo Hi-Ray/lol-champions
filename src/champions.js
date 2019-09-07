ba168eae532359fbc426449b279e9576cb0dd855
Ray <dev@hiray.me>
2019-09-07 04:17:50 +0100

    Implemented locales

--- src/champions.js
+++ src/champions.js
@@ -1,66 +1,108 @@
 const {get} = require('request-promise');
-const {writeFile} = require('fs');
+const {writeFile, existsSync, mkdirSync} = require('fs');
 const {join} = require('path');
-
 const log4js = require('log4js');
 
 const logger = log4js.getLogger('champions');
 
 logger.level = 'debug';
 
-let champions = [];
-let key = [];
+let locales = [
+  'cs_cz',
+  'de_de',
+  'default',
+  'el_gr',
+  'en_au',
+  'en_gb',
+  'es_ar',
+  'es_es',
+  'es_mx',
+  'fr_fr',
+  'hu_hu',
+  'it_it',
+  'ja_jp',
+  'ko_kr',
+  'pl_pl',
+  'pt_br',
+  'ro_ro',
+  'ru_ru',
+  'th_th',
+  'tr_tr',
+  'vn_vn',
+  'zh_cn',
+  'zh_tw',
+];
 
-// Here you can change file names
-let championsFileName = 'champions';
-let idsFileName = 'ids';
-//
+/////////////////////////////////////////////
+/// Here you can change file/folder names ///
+let championsFileName = 'champions';      ///
+let championsFolderName = 'champions';    ///
+let idsFileName = 'ids';                  ///
+let idsFolderName = 'ids';                ///
+/////////////////////////////////////////////
 
-// Get and parse the data from ddragon
-function getChampionsJSON(version) {
-  get(`http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`).
+function generateJsonFiles() {
+  existsSync(championsFolderName) ?
+      logger.info(`${championsFolderName} exists continuing`) :
+      mkdirSync(championsFolderName);
+  existsSync(idsFolderName) ?
+      logger.info(`${idsFolderName} exists continuing`) :
+      mkdirSync(idsFolderName);
+  for (let i = 0; i < locales.length; i++) {
+    getChampionsJSON(locales[i].toLowerCase());
+  }
+}
+
+// Get and parse the data from cdragon
+function getChampionsJSON(locale) {
+  get(`http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/${locale}/v1/champion-summary.json`).
       then(data => {
-        let championsData = JSON.parse(data).data;
+        let json = JSON.parse(data);
+        let champions = new Array();
+        let key = new Array();
 
-        for (let value in championsData) {
-          champions.push(value);
-          key.push(championsData[value].key);
+        // Start from index 1 since index 0 isnt an actually champion its just None
+        for (let i = 1; i < json.length; i++) {
+          champions.push(json[i].name);
+          key.push(json[i].id);
         }
+
+        makeChampionsJSON(locale, champions, key);
+        makeIdsJSON(locale, champions, key);
       }).catch(err => {
     logger.error(err);
-  }).finally(() => {
-    makeChampionsJSON();
-    makeIdsJSON();
   });
 }
 
 // turn data into object then turn object into json string then write to file
-function makeIdsJSON() {
+function makeIdsJSON(locale, championArray, idArray) {
   let ids = {};
-  for (let i = 0; i < champions.length; i++) {
-    ids[champions[i]] = key[i];
+  for (let i = 0; i < championArray.length; i++) {
+    ids[championArray[i]] = idArray[i];
   }
-  logger.info(`Writing ${idsFileName}.json`);
-  writeFile(`${idsFileName}.json`, JSON.stringify(ids), err => {
-    if (err) {
-      logger.error(`Unable to write ${idsFileName}.json`);
-    }
-    logger.info(`Created ${idsFileName}.json`);
-  });
+
+  writeFile(join(idsFolderName, `${idsFileName}-${locale}.json`),
+      JSON.stringify(ids), err => {
+        if (err) {
+          logger.error(`Unable to write ${idsFileName}-${locale}.json`);
+        }
+        logger.info(`Created ${idsFileName}-${locale}.json`);
+      });
 }
 
-function makeChampionsJSON() {
+function makeChampionsJSON(locale, championArray, idArray) {
   let champion = {};
-  for (let i = 0; i < champions.length; i++) {
-    champion[key[i]] = champions[i];
+  for (let i = 0; i < championArray.length; i++) {
+    champion[idArray[i]] = championArray[i];
   }
-  logger.info(`Writing ${championsFileName}.json`);
-  writeFile(`${championsFileName}.json`, JSON.stringify(champion), err => {
-    if (err) {
-      logger.error(`Unable to write ${championsFileName}.json`);
-    }
-    logger.info(`Created ${championsFileName}.json`);
-  });
+
+  writeFile(join(championsFolderName, `${championsFileName}-${locale}.json`),
+      JSON.stringify(champion), err => {
+        if (err) {
+          logger.error(`Unable to write ${championsFileName}-${locale}.json`);
+        }
+        logger.info(`Created ${championsFileName}-${locale}.json`);
+      });
 }
 
-module.exports = {getChampionsJSON};
+module.exports = {generateJsonFiles};
--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 const log4js = require('log4js');
 const {get} = require('request-promise');
-const {getChampionsJSON} = require('./champions');
+const {generateJsonFiles} = require('./champions');
 
 const logger = log4js.getLogger('main');
 
 logger.level = 'debug';
 
-function task(version) {
-  getChampionsJSON(version);
-}
-
-// Get latest version from ddragon.
-get('http://ddragon.leagueoflegends.com/api/versions.json').then(data => {
-  logger.info('Fetching latest version.');
-  let latestVersion = JSON.parse(data)[0];
-  logger.info(`Latest version: ${latestVersion}`);
-  task(latestVersion);
-});
+generateJsonFiles();
